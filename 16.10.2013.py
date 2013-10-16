241e287278fe9b305a1f4b84e76569288249c942
opilane <opilane@PC215-10.(none)>
2013-10-16 11:56:06 +0300

    16.10.2013.py

---
+++ 16.10.2013.py
@@ -0,0 +1,51 @@
+print "tere"
+print 1+2
+print 2+6
+opilane = "Timo"
+if opilane == "Mariin":
+   print "Eksam:5"
+elif opilane == "Timo":
+   print ":)"
+else:
+   print "Eksam:3"
+
+x=0
+
+while x<10:
+   print "x=" +str(x)
+   x=x+1
+
+print "valmis. x=" + str(x)
+
+massiiv = ["a","b","c"]
+
+for element in massiiv:
+    print element
+
+massiiv = ["a", "b", "c", "d", "e"]
+
+for element in massiiv:
+    if element == "b":
+        continue
+    print element
+    if element == "c":
+        break
+
+print "valmis"
+
+x=0,0
+while x<0.8:
+    print "x=" +str(x) 
+    x = x + 0,1
+print "x=" +str(x)
+
+
+
+
+    
+    
+
+
+
+ 
+
