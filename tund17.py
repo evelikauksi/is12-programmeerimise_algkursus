53e609b09c2e44909841e09e53a450e24b9b7ea7
opilane <opilane@PC215-10.(none)>
2013-10-17 10:00:45 +0300

    tund17.py

---
+++ tund17.py
@@ -0,0 +1,23 @@
+
+
+x=0
+while x<10:
+       print "{0:02d}".format(x),
+       x= x + 1 
+print""
+
+
+x=0
+while x<20:
+       print "{0:02d}".format(x),
+       x= x + 1 
+print""
+
+
+
+
+x=0
+while x<30:
+       print "{0:02d}".format(x),
+       x= x + 1 
+print""
