6c3ccf4798e226e8b27e820c311a00eec4465f29
opilane <opilane@PC215-10.(none)>
2013-10-17 08:46:13 +0300

    kodutoo16.py

---
+++ kodutoo16.py
@@ -0,0 +1,26 @@
+x=int(-1)
+while x<99:
+        x =(x + 1)
+        x=str(x)
+        print(x).zfill(2),
+        x=int(x)
+        if x==9:
+            print""
+        if x==19:
+            print""
+        if x==29:
+            print""
+        if x==39:
+            print""
+        if x==49:
+            print""
+        if x==59:
+            print""
+        if x==69:
+            print""
+        if x==79:
+            print""
+        if x==89:
+            print""
+        if x==99:
+            print""
