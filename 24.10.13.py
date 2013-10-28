6b37df5fc06e22c4133a360931560f8ed59e52ad
evelikauksi <eveli.kauksi@gmail.com>
2013-10-28 17:30:19 +0200

    kodutoo

---
+++ 24.10.13.py
@@ -0,0 +1,15 @@
+def leian_keskmise (a,b,c,d,e):
+	return (a+b+c+d+e)/5
+print leian_keskmise (1,2,3,4,5)
+
+def fibonacci (a,b,c,d,e):
+	return (a+b), (b+c), (c+d), (d+e)
+print fibonacci (1,2,3,4,5)	
+
+def ruutvorrand (a,b,c):
+	return ((-b)+((b**2)-(4*a*c))**0.5)/(2*a)
+print ruutvorrand (5,9,-2)	
+
+def ruutvorrand (a,b,c):
+	return ((-b)-((b**2)-(4*a*c))**0.5)/(2*a)
+print ruutvorrand (5,9,-2)	
