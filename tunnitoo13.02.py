9d10c135368f7a9f0f804d41dfa584ee86aa2e78
evelikauksi <eveli.kauksi@gmail.com>
2014-02-13 11:24:03 +0200

    Create tunnitoo13.02.py

---
+++ tunnitoo13.02.py
@@ -0,0 +1,17 @@
+list = ["a" , "b" , "c"]
+
+list.append ("a")
+
+list.insert (4 , "d")
+
+list.remove ("a")
+
+list.pop (2)
+
+print list.index ("b")
+
+print list.count ("c")
+
+list.sort ()
+
+list.reverse ()
